89312f5a40721214da8a988188003a446bbb57b9
AnhDT <gracefulgirl207@gmail.com>
2016-09-28 17:56:23 +0700

    first commit

---
+++ basic/app.jsx
@@ -0,0 +1,34 @@
+function Application(props) {
+    return (
+        <div className="scoreboard">
+            <div classname="header">
+                <h1>{props.title}</h1>
+            </div>
+            <div className="players">
+                <div className="player">
+                    <div className="player-name">Jim Hokins</div>
+                    <div className="player-score">
+                        <div className="counter">
+                            <button className="counter-action decrement"> - </button>
+                            <div className="counter-score"> 31 </div>
+                            <button className="counter-action increment"> + </button>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="player">
+                    <div className="player-name">Andrew</div>
+                    <div className="player-score">
+                        <div className="counter">
+                            <button className="counter-action decrement"> - </button>
+                            <div className="counter-score"> 34 </div>
+                            <button className="counter-action increment"> + </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+ReactDOM.render(<Application title="My scoreboard" />, document.getElementById('container'));
